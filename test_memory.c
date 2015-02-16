929277eed4c37842f6df05fb5e6356ca76bc9363
linxuping <linxuping@126.com>
2015-02-16 15:31:35 +0800

    get_memory test

--- test_memory.c
+++ test_memory.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void swap_test_wild_pt(int* p1,int* p2);
+char* get_memory();
 
 int main()
 {
     int a=1;
     int b=2;
-    swap_test_wild_pt(&a, &b);
+    //swap_test_wild_pt(&a, &b);
+    //
+    char *p = get_memory();
+    printf("new memory: %s \n",p); //not *p
+    delete[] p;
     return 0;   
 }
 
@@ -22,4 +29,11 @@ void swap_test_wild_pt( int* p1,int* p2 )
     *p2 =*p;
 }
 
+char* get_memory()
+{
+    char *p = (char*)malloc(sizeof(char)*4);
+    //*p = "123"; //why not  ?????
+    strcpy(p,"123");
+    return p;
+}
 
