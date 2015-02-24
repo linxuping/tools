246775574a33585ca052f5c43d483f69d9009ac5
linxuping <linxuping@126.com>
2015-02-24 21:49:47 +0800

    const class member pt

--- test_const.c
+++ test_const.c
@@ -2,6 +2,7 @@
 
 const int get_const_int();
 const char* get_const_str();
+void test_class_const_function();
 class Test1;
 
 int main()
@@ -23,6 +24,8 @@ int main()
     //4.   for pointer
     const char* p3 = get_const_str();
     //char* p4 = get_const_str(); //error !
+    //5.   const class member function
+    test_class_const_function();
     return 0;
 }
 
@@ -37,9 +40,16 @@ const char* get_const_str(){
     return "test";
 }
 
+class Item
+{
+public:
+    int m_val;
+};
 class Test1
 {
 public:
+    Test1():m_pitem(new Item){}
+    ~Test1(){ delete m_pitem; }
     int m_test;
     mutable int m_mt;
     void Push(int elem);
@@ -48,14 +58,23 @@ public:
 private:
     int m_num;
     int m_data[100];
+    Item* m_pitem;
 };
 int Test1::GetCount(void) const
 {
     //++m_num; // 编译错误，企图修改数据成员m_num
     //Pop(); // 编译错误，企图调用非const 函数
     m_mt = 100; //ok, for it is mutable
+    //
+    //m_pitem = NULL; // member ‘Test1::m_pitem’ in read-only object
+    m_pitem->m_val = 101;
+
     return m_num;
 }
 
-
+void test_class_const_function()
+{
+    Test1 t1;
+    t1.GetCount();
+}
 
