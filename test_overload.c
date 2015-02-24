c07c8b06a9d997e978c7839eea28c7431460db84
linxuping <linxuping@126.com>
2015-02-24 20:21:53 +0800

    overload

---
+++ test_overload.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/*
+int get_ret();
+float get_ret();
+*/
+
+int main()
+{
+    /*int m = get_ret();
+    float n = get_ret();*/
+    return 0;
+}
+
+//------ rel -------
+/*
+int get_ret(){
+    printf("return int \n");
+}
+float get_ret(){
+    printf("return float \n");
+}
+*/
+
+
