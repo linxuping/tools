b98396da16eb69d738fc7e33f88f58f193264cc7
linxuping <linxuping@126.com>
2015-02-26 18:40:29 +0800

    c type

--- test_common.c
+++ test_common.c
@@ -5,6 +5,7 @@ void test_swap();
 void test_class_obj();
 void test_operator();
 void test_switch();
+void test_type();
 
 int main()
 {
@@ -13,10 +14,11 @@ int main()
     test_class_obj();
     test_operator();
     test_switch();
+    test_type();
     return 0;    
 }
 
-//---- rel ---->
+//---- ---- rel ---- ---->
 int num = 1;
 void test_domain()
 {
@@ -29,6 +31,13 @@ void test_class_obj()
 {
     Test1 t1;
     Test1 t2(); //both are ok.
+}
+
+void test_type()
+{
+    unsigned int a = 6;
+    int b = -6;
+    printf("uint 6 + int -6 = %d \n",a+b);
 }
 
 void swap_1(int& a, int& b)
