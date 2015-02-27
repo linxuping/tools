adfa287712da4f3e7f8ca3c5a6036392cd6f8b10
linxuping <linxuping@126.com>
2015-02-27 10:18:38 +0800

    childp exit but not ret

--- test_fork.c
+++ test_fork.c
@@ -6,8 +6,8 @@ void test_vfork2();
 
 int main(void)
 {
-    //test_vfork1();
-    test_vfork2();
+    test_vfork1();
+    //test_vfork2();
     return 0;
 }
 
@@ -15,13 +15,16 @@ int main(void)
 
 
 //---- ---- rel ---- ---->
-void test_vfork1() //why coredump.
+void test_vfork1() 
 {
     pid_t pid;
     int count=0;
+    printf("vfork1 parent pid:%d\n",getpid());
     pid=vfork();
     count++;
-    printf("vfork1 count= %d\n",count);
+    printf("vfork1 pid:%d count= %d\n",getpid(),count);
+    fflush(stdout);
+    //_exit(0); //exit but not return
 }
 
 void test_vfork2()
