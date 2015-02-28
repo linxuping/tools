a603da298c8ea47f35d2740eade67889de90e85a
linxuping <linxuping@126.com>
2015-02-28 10:16:37 +0800

    class const research

--- test_const.c
+++ test_const.c
@@ -3,6 +3,7 @@
 const int get_const_int();
 const char* get_const_str();
 void test_class_const_function();
+void test_class_const();
 class Test1;
 
 int main()
@@ -26,6 +27,8 @@ int main()
     //char* p4 = get_const_str(); //error !
     //5.   const class member function
     test_class_const_function();
+    //
+    test_class_const();
     return 0;
 }
 
@@ -77,4 +80,41 @@ void test_class_const_function()
     Test1 t1;
     t1.GetCount();
 }
+
+class Test2{
+public:
+    Test2():m(1){ n=2; }
+    const int m;
+    int n;
+    //
+    void run1(){}
+    void run2()const {}
+};
+void test_class_const()
+{
+    /*
+    1\const对象rodata?  -> no
+    2\const成员函数和非const成员函数存储位置 ->no but .text
+    3\const对象访问成员函数必须找到'对应的含有const的全局符号'? ->no, 应该是先隐式转换和匹配函数
+    const变量 vs 常量区？
+    */
+
+    const Test2 t21; 
+    printf("%s const class obj add:%p \n", __FUNCTION__,&t21);
+    printf("%s class const mem fun:%p \n", __FUNCTION__,&Test2::run2);
+    //printf("%s class mem fun:%p \n", &(t21.run1)); //错误：ISO C++ 不允许通过取已绑定的成员函数的地址来构造成员函数指针。请改用‘&Test2::run2’ [-fpermissive]
+    printf("%s class       mem fun:%p \n", __FUNCTION__,&Test2::run1);
+
+    //t21.run1(); //将‘const Test2’作为‘void Test2::run1()’的‘this’实参时丢弃了类型限定   const难转非const? 非const易转const?
+    t21.run2();
+
+    Test2 t22; 
+    t22.run1();
+    t22.run2();
+
+    const Test2* pt21 = &t22; //没问题
+    //Test2* pt22 = &t21; //从类型‘const Test2*’到类型‘Test2*’的转换无效
+    Test2* pt23 = const_cast<Test2*>(&t21); //从类型‘const Test2*’到类型‘Test2*’的转换无效
+}
+
 
