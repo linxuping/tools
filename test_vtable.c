b8d947be68aff26f60330d207713ccbfc78c3a0b
linxuping <linxuping@126.com>
2015-03-02 13:48:34 +0800

    vptr when obj assign & space of vptr\vbtr

--- test_vtable.c
+++ test_vtable.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+void test_obj_assign();
+void test_vptr_vbtr();
 
 class Base{
 public:
@@ -30,5 +32,46 @@ int main()
     pfun2 = (PFUN2)*((int*)*(int*)pb2+1);  //fuck, run Base::private function, like pb2->run_private()
     pfun2();
     //
+    test_obj_assign();
+    //
+    test_vptr_vbtr();
     return 0;
 }
+
+//---- rel ---->
+void test_obj_assign()
+{
+    printf("%s run\n",__FUNCTION__);
+    Derived de;
+    Base be = de; //vptr直接覆盖，还是operator=会再更新be的vptr ? 和c++模型里面讲的对照
+    de.run();
+    //Derived de2 = static_cast<Derived>(be);
+    //de2.run();
+}
+
+class Base2{
+public:
+    int m;
+};
+class Derived2:virtual public Base2{
+public:
+    virtual void run(){ printf("Derived2::run() \n"); }
+};
+void test_vptr_vbtr()
+{
+    printf("%s Derived2 size:%d 12? \n",__FUNCTION__,sizeof(Derived2));
+    Derived2 *pt = new Derived2();
+    pt->m = 1;
+    printf("%s Derived2 m:%d  \n",__FUNCTION__,*((int*)pt+1));
+    //(*((int*)*(int*)pt))(); //‘*(int*)(*(int*)pt)’不能用作函数  不是随便的指针都能做函数使
+    ( (PFUN2)*((int*)*(int*)pt) )();
+    //printf("%s Derived2 m:%p vtable -1:%p  \n",__FUNCTION__,&pt->m,(int*)*((int*)*((int*)pt)-1) );
+    delete pt; 
+
+    /*coredump ?
+    Base2 *pt2 = new Derived2(); 
+    //Base2  virtual ~Base2(){} // must added, why?
+    delete pt2;
+    */
+}
+
