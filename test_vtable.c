c05fa06341f12cc915ce86ef7086587e0f2ebf9c
linxuping <linxuping@126.com>
2015-03-18 17:39:24 +0800

    vptr vbtr research

--- test_vtable.c
+++ test_vtable.c
@@ -63,7 +63,7 @@ public:
 };
 class Derived21:virtual public Base2{};
 class Derived22:virtual public Base2{};
-class Derived2:virtual public Derived21, virtual public Derived22{
+class Derived2:public Derived21, public Derived22{
 public:
     virtual void run(){ printf("Derived2::run() \n"); }
 };
@@ -72,8 +72,15 @@ void test_vptr_vbtr()
     printf("%s Derived2 size:%d 12=member+vptr+vbtr? \n",__FUNCTION__,sizeof(Derived2)); //vbtr就能够辨识出m_a
     Derived2 *pt = new Derived2();
     pt->m_a = 100;
-    printf("%s Derived2 m_a:%d  \n",__FUNCTION__,*((int*)pt+1));
-    printf("%s Derived2 base virtual offset:%d ??? \n",__FUNCTION__,(int)*( (int*)*((int*)pt+2) -1 ));
+/* ????
+d21
+d22
+vbtr
+*/
+    printf("%s Derived2 0:%d - %p     (有virtual时这是一个vptr，无时这是啥?) \n",__FUNCTION__,*((int*)pt+0),(int*)pt);
+    printf("%s Derived2 1:%d - %p  \n",__FUNCTION__,*((int*)pt+1),(int*)pt+1);
+    printf("%s Derived2 2:%d  \n",__FUNCTION__,*((int*)pt+2));
+    printf("%s Derived2 base virtual offset:%d ??? \n",__FUNCTION__,(int)*( (int*)*((int*)pt+0) ));
     //(*((int*)*(int*)pt))(); //‘*(int*)(*(int*)pt)’不能用作函数  不是随便的指针都能做函数使
     ( (PVFUN)*((int*)*(int*)pt) )();
     delete pt; 
@@ -126,50 +133,82 @@ void test_multi_inheritance()
 
 class B{
 public:
-    virtual void f(){}
-    void bf(){}
+    virtual void f(){ printf("B::f \n"); }
+    void bf(){ printf("B::bf \n"); }
 
+    B():ib(1),cb(2){}
     int ib;
     int cb;
 };
 class B1:virtual public B{
 public:
-    void f(){}
-    virtual void f1(){}
-    void bf1(){}
+    void f(){ printf("B1::f \n"); }
+    virtual void f1(){ printf("B1::f1 \n"); }
+    void bf1(){ printf("B1::bf1 \n"); }
 
+    B1():ib1(3),cb1(4){}
     int ib1;
     int cb1;
 };
 class B2:virtual public B{
 public:
-    void f(){}
-    virtual void f2(){}
-    void bf2(){}
+    void f(){ printf("B2::f \n"); }
+    virtual void f2(){ printf("B2::f2 \n"); }
+    void bf2(){ printf("B2::bf2 \n"); }
 
+    B2():ib2(5),cb2(6){}
     int ib2;
     int cb2;
 };
 class D:public B1,public B2{
 public:
     //D():ib(1),cb(2),ib1(3),cb1(4),ib2(5),cb2(6),id(7),cd(8){} 错误：类‘D’没有名为‘ib’...的字段
-    void f(){}
-    void f1(){}
-    void f2(){}
-    void df(){}
+    void f(){ printf("D::f \n"); }
+    void f1(){ printf("D::f1 \n"); }
+    void f2(){ printf("D::f2 \n"); }
+    virtual void df(){ printf("D::df \n"); } //加virtual会不会增加一个vptr ?
 
-    int id;
+    D():id(7),cd(8){}
     int cd;
+    int id;
 };
 void test_vir_inheritance_complex()
-{
+{/*
+0: 134516588  --vptr
+1: 3 
+2: 4 
+
+3: 134516612  --vptr
+4: 5 
+5: 6 
+
+6: 8 
+7: 7 
+
+8: 134516632  --vbtr
+9: 1 
+10: 2
+*/
     //refer: http://coolshell.cn/articles/12176.html
     printf("%s D size:%d =?= [B1/B2 in D:(vptr)4+4+4]*2+(vbtr)4+4+4 \n",__FUNCTION__,sizeof(D) );
     D d;
     for(int i=0; i<11; ++i)
         printf("%d: %d \n",i,*((int*)&d+i) );
-}
+    printf("虚继承+虚函数 内存布局:  \n");
+    printf("vptr1:  \n");
+    ( (PVFUN)*(int*)*((int*)&d+0) )();       //f   ==>   B1-vptr + D-vptr
+    ( (PVFUN)*( (int*)*((int*)&d+0)+1 ) )(); //f1
+    ( (PVFUN)*( (int*)*((int*)&d+0)+2 ) )(); //f2
+    ( (PVFUN)*( (int*)*((int*)&d+0)+3 ) )(); //df
+    //( (PVFUN)*( (int*)*((int*)&d+0)+4 ) )();
 
+    printf("vptr2:  \n");
+    ( (PVFUN)*(int*)*((int*)&d+3) )();       //f
+    ( (PVFUN)*( (int*)*((int*)&d+3)+1 ) )(); //f2
+    //( (PVFUN)*( (int*)*((int*)&d+3)+2 ) )();
+
+    //( (PVFUN)*(int*)*((int*)&d+8) )(); //test vbtr
+}
 
 
 void test_this()
