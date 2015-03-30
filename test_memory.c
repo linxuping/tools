1b3b3444995ed358fa4f7f9b033e45c9641cfe5b
linxuping <linxuping@126.com>
2015-03-30 17:57:33 +0800

    mmap direction like stack

--- test_memory.c
+++ test_memory.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "common.h"
 
 void swap_test_wild_pt(int* p1,int* p2);
 char* get_memory_heap();
 char* get_memory_stack();
 void get_memory_temp_pt(char *buf);
+void test_mmap_address();
 
 int main()
 {
@@ -26,6 +28,7 @@ int main()
     get_memory_temp_pt(buf);
     printf("after,  buf:%s %p \n",buf,&buf);
     //
+    test_mmap_address();
     return 0;   
 }
 
@@ -63,4 +66,27 @@ void get_memory_temp_pt(char *buf)
     strcpy(buf,"789");
     printf("%s, buf:%s %p \n",__FUNCTION__,buf,&buf);
 }
+
+void test_mmap_address()
+{
+    ENTER_TEST();
+    void *pt1 = malloc(128*2*1024);
+    void *pt2 = malloc(128*2*1024);
+    printf("pt1:%p < \npt2:%p ?\n",pt1,pt2);
+    void *pt3 = malloc(12*1024);
+    void *pt4 = malloc(12*1024);
+    printf("pt3:%p < \npt4:%p ?\n",pt3,pt4);
+    free(pt1);
+    free(pt2);
+    free(pt3);
+    free(pt4);
+/*
+pt1:0xb7f1c008 higher
+pt2:0xb7edb008 
+
+pt3:0x841b018 
+pt4:0x841e020 higher
+*/
+}
+
 
