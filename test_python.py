920680ead074734d37967d6394c8f0c1c814e5bc
linxuping <linxuping@126.com>
2015-04-09 15:53:46 +0800

    python test

---
+++ test_python.py
@@ -0,0 +1,28 @@
+
+#test_loop_count() #cannot find here
+
+def test_loop_count():
+    lis=[1,2,3,4]*10000000
+    #print len(lis) #0.280s
+    for item in lis: #0.95s
+        pass
+    #so get len better than loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+test_loop_count()
