651fb1cce22269c53ffdb32847d07706cdf9df4b
linxuping <linxuping@126.com>
2015-04-10 14:51:30 +0800

    cast review

--- test_cast.c
+++ test_cast.c
@@ -6,7 +6,7 @@ void test_static_cast();
 
 int main()
 {
-	test_const_cast();
+    test_const_cast();
     test_reinterpret_cast();
     test_static_cast();
 }
@@ -28,25 +28,26 @@ void test_static_cast()
 }
 class Test1
 {
-public:
-    int m_test = 1;
+    public:
+        int m_test;
 };
 void test_const_cast()
 {
-	Test1 t1;
+    Test1 t1;
     const Test1* p1 = &t1;	//new Test1   read-only object
-	//p1->m_test = 1;  //error
-	//-- cast to non-const -->
-	Test1* p2 = const_cast<Test1*>(p1);
-	p2->m_test = 1001;
-	printf("Test1::m_test %d\n",p1->m_test);
-	//-- cast to const -->
-	const Test1* p3 = const_cast<const Test1*>(p2);
-	//p3->m_test = 1002; //error 
+    //p1->m_test = 1;  //error
+    //-- cast to non-const -->
+    Test1* p2 = const_cast<Test1*>(p1); //仅仅const_cast有能力cast away
+    //Test1* p20 = static_cast<Test1*>(p1); //==== ==== ==== ==== >错误：从类型‘const Test1*’到类型‘Test1*’的 static_cast 丢失了限定符
+    p2->m_test = 1001;
+    printf("Test1::m_test %d\n",p1->m_test);
+    //-- cast to const -->
+    const Test1* p3 = const_cast<const Test1*>(p2);
+    //p3->m_test = 1002; //error 
 }
 void test_reinterpret_cast()
 {
-	Test1 t1;
+    Test1 t1;
     void* p1 = reinterpret_cast<void*>(&t1);	//new Test1   read-only object
     printf("Test*:%p void*:%p \n",&t1,p1);
 }
