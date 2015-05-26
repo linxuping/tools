3f28a1776c7abdb7574e601bb4c673505ce82a8a
linxuping <linxuping@126.com>
2015-05-26 22:54:47 +0800

    getattr

--- test_python.py
+++ test_python.py
@@ -8,21 +8,20 @@ def test_loop_count():
         pass
     #so get len better than loop
 
+class Test:
+    def __init__(self):
+        fun = getattr(self,"run_xx") 
+        fun()
+    def run_xx(self):
+        print "i am Test::run()"
+
+def test_class_fun_obj():
+    tt = Test()
+    fun = getattr(tt,"run_xx")
+    fun()
 
 
 
+#test_loop_count()
+test_class_fun_obj()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-test_loop_count()
