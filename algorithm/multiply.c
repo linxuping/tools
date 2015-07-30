77cec226428a5c87d1012a1a3a281a3564e5a43e
linxuping <linxuping@126.com>
2015-07-30 17:13:43 +0800

    multiply

---
+++ algorithm/multiply.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	char left[] = "12345678910";
+	char right[] = "9876543210";
+
+	int *tmp1 = new int(strlen(left)+strlen(right));
+	int *tmp2 = new int(strlen(left)+strlen(right));
+	return 0;
+}
+
