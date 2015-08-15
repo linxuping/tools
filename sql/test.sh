bf9e2ed59143807aef5fd110b31bffc29f52d8d1
linxuping <linxuping@126.com>
2015-08-15 15:33:38 +0800

    right join

--- sql/test.sh
+++ sql/test.sh
@@ -9,7 +9,10 @@ drop table ucinfo;
 
 create table `ucinfo` ( `id` int(11) NOT NULL AUTO_INCREMENT, `number` int(11) DEFAULT 0, PRIMARY KEY (`id`) );
 
-create table `uclog` ( `id` int(11) NOT NULL AUTO_INCREMENT, `ucid` int(11) NOT NULL,`number` int(11) DEFAULT 0, PRIMARY KEY (`id`), CONSTRAINT `ucid_1234` FOREIGN KEY (`ucid`) REFERENCES `ucinfo` (`id`) );
+create table `uclog` ( `id` int(11) NOT NULL AUTO_INCREMENT, `ucid` int(11) NOT NULL,`ip` char(48) DEFAULT 0, PRIMARY KEY (`id`), CONSTRAINT `ucid_1234` FOREIGN KEY (`ucid`) REFERENCES `ucinfo` (`id`) );
+
+insert into ucinfo (number) values ('1234567'),('1234568'),('1234569');
+insert into uclog (ucid,ip) values (1,"192.168.12.1"), (2,"192.168.12.2"), (2,"192.168.12.2"), (2,"192.168.12.3"), (3,"192.168.12.2"), (3,"192.168.12.4");
 
 EOF
 
