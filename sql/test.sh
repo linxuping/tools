cca4685c2996be6d34ec2aac593f488ead9998b6
linxuping <linxuping@126.com>
2015-08-15 12:03:18 +0800

    create tables

--- sql/test.sh
+++ sql/test.sh
@@ -3,5 +3,13 @@ mysql -uroot <<EOF
 show databases;
 use salmon;
 #show tables;
+
+drop table uclog;
+drop table ucinfo;
+
+create table `ucinfo` ( `id` int(11) NOT NULL AUTO_INCREMENT, `number` int(11) DEFAULT 0, PRIMARY KEY (`id`) );
+
+create table `uclog` ( `id` int(11) NOT NULL AUTO_INCREMENT, `ucid` int(11) NOT NULL,`number` int(11) DEFAULT 0, PRIMARY KEY (`id`), CONSTRAINT `ucid_1234` FOREIGN KEY (`ucid`) REFERENCES `ucinfo` (`id`) );
+
 EOF
 
