9bb3adb0ad6fd6a1c96b2e31a8020b81ae1b20db
linxuping <linxuping@126.com>
2016-03-24 14:39:30 +0800

    mon test

---
+++ mongodb/mymongo.py
@@ -0,0 +1,29 @@
+from pymongo import *  
+#from pymongo.objectid import ObjectId  
+from gridfs import *
+from pprint import pprint  
+  
+conn = MongoClient("192.168.29.163", 27017)
+db = conn.test  
+print db
+db = conn.collections  
+print db
+gfs = GridFS(db)
+print gfs
+
+print "+ write in"
+with open("/etc/rc.local", "r") as file:  
+	id = gfs.put(file.read(), filename = "/xxx/yyy/a.txt", upload = "q.yuhen", abc = 123)  
+	print id 
+print gfs.list()
+
+print "+ read out"
+with open("/tmp/rc.local", "w") as file:  
+	out = gfs.get_last_version("/xxx/yyy/a.txt")  
+	file.write(out.read())  
+	print out.name, out.length 
+
+print gfs.list()
+
+
+
