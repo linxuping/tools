cbf7d3594d71c45cdb0517ad5464da44071265e5
linxuping <linxuping@126.com>
2016-03-25 17:37:35 +0800

    flock test

---
+++ linux/flock.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+	if (argc > 1) {
+		int fd = open(argv[1], O_WRONLY);
+		if(fd == -1) {
+			printf("Unable to open the file\n");
+			exit(1);
+		}
+		static struct flock lock;
+
+		lock.l_type = F_WRLCK;
+		lock.l_start = 0;
+		lock.l_whence = SEEK_SET;
+		lock.l_len = 0;
+		lock.l_pid = getpid();
+
+		int ret = fcntl(fd, F_SETLKW, &lock);
+		printf("Return value of fcntl:%d\n",ret);
+		if(ret==0) {
+			while (1) {
+				scanf("%c", NULL);
+			}
+		}
+	}
+}
+
+/*
+使用mount命令带“mand”参数来重新挂载根文件系统，如下所示。这将在文件系统级别使能强制锁功能,对于“mandatory.txt”使能Set-Group-ID，同时不使能Group-Execute-Bit.
+# mount -oremount,mand /
+# touch advisory.txt
+# touch mandatory.txt
+# chmod g+s,g-x mandatory.txt
+
+# ./file_lock advisory.txt   -->  other file use advisory.txt will block.
+# ./file_lock mandatory.txt   -->  other file use mandatory.txt will block.
+*/
+
