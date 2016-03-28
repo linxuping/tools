29d7d7c2683aac0f5202690327077cc105b8dc87
linxuping <linxuping@126.com>
2016-03-28 11:15:54 +0800

    geo index

--- mongodb/mymongo.py
+++ mongodb/mymongo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from pymongo import *  
 #from pymongo.objectid import ObjectId  
 from gridfs import *
@@ -26,5 +28,26 @@ with open("/tmp/rc.local", "w") as file:
 
 print gfs.list()
 
-
+db.places.insert({
+"loc": {"type": "Point", "coordinates": [ -73.97, 40.77 ] },
+"name": "Central Park",
+"category" : "Parks"
+   }
+)
+db.places.insert({
+      "loc" : { "type": "Point", "coordinates": [ -73.88, 40.78 ] },
+      "name": "La Guardia Airport",
+      "category" : "Airport"
+   }
+)
+#建立索引
+#print type(db.places),dir(db.places)
+'''
+['_BaseObject__codec_options', '_BaseObject__read_concern', '_BaseObject__read_preference', '_BaseObject__write_concern', '_Collection__create', '_Collection__create_index', '_Collection__database', '_Collection__find_and_modify', '_Collection__full_name', '_Collection__name', '__call__', '__class__', '__delattr__', '__dict__', '__doc__', '__eq__', '__format__', '__getattr__', '__getattribute__', '__getitem__', '__hash__', '__init__', '__iter__', '__module__', '__ne__', '__new__', '__next__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_command', '_count', '_delete', '_insert', '_insert_one', '_legacy_write', '_socket_for_primary_reads', '_socket_for_reads', '_socket_for_writes', '_update', 'aggregate', 'bulk_write', 'codec_options', 'count', 'create_index', 'create_indexes', 'database', 'delete_many', 'delete_one', 'distinct', 'drop', 'drop_index', 'drop_indexes', 'ensure_index', 'find', 'find_and_modify', 'find_one', 'find_one_and_delete', 'find_one_and_replace', 'find_one_and_update', 'full_name', 'group', 'index_information', 'initialize_ordered_bulk_op', 'initialize_unordered_bulk_op', 'inline_map_reduce', 'insert', 'insert_many', 'insert_one', 'list_indexes', 'map_reduce', 'name', 'next', 'options', 'parallel_scan', 'read_concern', 'read_preference', 'reindex', 'remove', 'rename', 'replace_one', 'save', 'update', 'update_many', 'update_one', 'with_options', 'write_concern']`
+'''
+db.places.create_index( [("loc","2dsphere")] )
+#参数不是1或-1，为2dsphere。还可以建立组合索引。
+#db.places.createIndex( { "loc" : "2dsphere" , "category" : -1, "name": 1 } )
+print db.places.find
+print db.places.list_indexes()
 
