10761944ca076457a936eb34b012da2a457bc1ec
linxuping <linxuping@126.com>
2016-04-12 14:36:51 +0800

    decorator testing

--- python/decorator.py
+++ python/decorator.py
@@ -1,19 +1,19 @@
 def deco(func):
     def _deco(a, b):
-        print("before myfunc() called.")
+        print("before myadd() called.")
         if a == 0:
-            return 0
+            return -9999
         ret = func(a, b)
-        print("  after  myfunc() called. result: %s" % ret)
+        print("  after  myadd() called. result: %s" % ret)
         return ret
     return _deco
  
 @deco
-def myfunc(a, b):
-    print(" myfunc(%s,%s) called." % (a, b))
+def myadd(a, b):
+    print(" myadd(%s,%s) called." % (a, b))
     return a + b
  
-#myfunc(1, 2) #myfun called.
-myfunc(0, 2)  #myfun will not be called.
+#print myadd(1, 2) #myfun called.
+print myadd(0, 2)  #myfun will not be called.  -9999
 
 
