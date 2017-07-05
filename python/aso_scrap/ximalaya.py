684f6dca75bb5f29fc25683c51f4fdf75438fb39
linxp <linxuping@126.com>
2017-07-05 23:37:23 +0800

    ximalaya

---
+++ python/aso_scrap/ximalaya.py
@@ -0,0 +1,63 @@
+﻿# -*- coding: utf-8 -*-
+import socket
+import urllib2
+import time
+import sys
+import os
+import json
+import struct
+timeout=10
+socket.setdefaulttimeout(timeout)
+import xml.dom.minidom
+import bs4
+from bs4 import BeautifulSoup
+
+urls = ["http://www.ximalaya.com/12495477/album/4633831"]
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}  
+def _get_doc(url):
+	req = urllib2.Request(url, headers=headers)  
+	socket = urllib2.urlopen(req)  
+	ret = socket.read()
+	return BeautifulSoup(ret, 'html.parser')
+def get_doc(url):
+	for x in xrange(3):
+		try:
+			return _get_doc(url)
+		except:
+			pass
+	return _get_doc(url)
+
+
+doc=get_doc("http://www.ximalaya.com/12495477/album/4633831")
+for ret in doc.find_all('a',class_='pagingBar_page'):
+	#a = ret.a['href'].split('appid/')[1].split('/')[0]
+	#b = ret.a.div.h5.string.split('.')[1]
+	#c = ret.a.div.find_all('h6')[1].find_all('span')[0].string
+	#d = ret.a.div.find_all('h6')[1].find_all('span')[1].string.strip('+')
+	url = "http://www.ximalaya.com"+ret['href']
+	if 'album' not in url:
+		continue
+	if not url in urls:
+		urls.append(url)
+
+print urls
+for url in urls:
+	doc=get_doc(url)
+	print url
+	for ret in doc.find_all('a',class_='title'):
+		if not 'href' in ret.attrs:
+			continue
+		link = ret['href']
+		if not 'sound' in link:
+			continue
+		voice = link.rsplit('/',2)[-1]
+		print voice
+		doc2=get_doc('http://m.ximalaya.com/tracks/%s.json'%voice)
+		if 'play_path' in doc2:
+			print doc2.split('"play_path":"')[1].split('"',2)[0]
+
+
+
+import time
+time.sleep(3000)
