ad5f2db552608ed1c159ebcd9da459f596440b0e
linxuping@126.com <linxuping@linxuping126comdeMacBook-Air.local>
2018-05-20 17:25:23 +0800

    select chan test

--- go/server.go
+++ go/server.go
@@ -1,38 +1,41 @@
 package main
+
 import (
-    "fmt"
-    "net/http"
-    "log"
-    "./mybase"
-    "./test"
+	"./mybase"
+	"./test"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", mybase.SayhelloName) //设置访问的路由
+	http.HandleFunc("/", mybase.SayhelloName) //设置访问的路由
 
-    if false {
-        //test.Test()
-        test.TestChan()
-        return
-    }
+	if true {
+		//test.RedisTest()
+		//test.Test()
+		//test.TestChan()
+		test.TestSelectChan()
+		//test.Test2ChanAndGo()
+		return
+	}
 
-    funcs := map[string]func(){"testfunc":test.Testfunc}
-    //test.Testfunc() //ok
-    //funcs['testfunc']() //error
-    //funcs[`testfunc`]() //ok
-    funcs["testfunc"]() //test.Testfunc()
+	funcs := map[string]func(){"testfunc": test.Testfunc}
+	//test.Testfunc() //ok
+	//funcs['testfunc']() //error
+	//funcs[`testfunc`]() //ok
+	funcs["testfunc"]() //test.Testfunc()
 
-    str := `
+	str := `
     你好!
     world`
-    fmt.Println(str)
+	fmt.Println(str)
 
-    go test.Coroutine1()
-    go test.Coroutine2()
-    err := http.ListenAndServe(":9090", nil) //设置监听的端口
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
-    log.Fatal("ListenAndServer 9090.")
+	go test.Coroutine1()
+	go test.Coroutine2()
+	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+	log.Fatal("ListenAndServer 9090.")
 }
-
--- go/test/channel.go
+++ go/test/channel.go
@@ -9,19 +9,19 @@ func fibonacci(c, quit chan int) {
     x, y := 1, 1
     for {
         select {
-        case c <- x: //send to c
-            x, y = y, x + y
-        case <-quit: //v := <-ch  // 从ch中接收数据，并赋值给v
-            fmt.Println("quit")
-            return
-        default:
-            fmt.Println("default and blocking.")
+            case c <- x:            //send to c
+                x, y = y, x + y
+            case <-quit:            //v := <-ch  // 从ch中接收数据，并赋值给v
+                fmt.Println("quit")
+                return
+            default:
+                fmt.Println("default and blocking.")
         }
         time.Sleep(time.Second/2)
     }
 }
 
-func TestChan() {
+func Test2ChanAndGo() {
     c := make(chan int)
     quit := make(chan int)
     go func() {
@@ -33,3 +33,30 @@ func TestChan() {
     }()
     fibonacci(c, quit)
 }
+
+func TestChan() {
+    c := make(chan int) 
+	fmt.Println("TestChan.") 
+    //c := make(chan int, 1) 
+    c <- 1
+	fmt.Println("step 1.")   //block ?
+    c <- 2
+	fmt.Println("end.") //block ?
+}
+
+func TestSelectChan() {
+    c := make(chan int, 1) 
+	fmt.Println("TestChan.") 
+    for {
+        select {
+            case c <- 1:             
+                fmt.Println("c <- 1.")
+            default:
+                fmt.Println("default and blocking.")
+        }
+        time.Sleep(time.Second)
+    }
+	fmt.Println("end.") 
+}
+
+
