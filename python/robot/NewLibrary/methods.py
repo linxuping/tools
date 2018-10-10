d74ab7cb19df91ac9e16ff12e7263e379302c85d
linxuping <linxuping@126.com>
2018-10-10 18:23:59 +0800

    Update methods.py

--- python/robot/NewLibrary/methods.py
+++ python/robot/NewLibrary/methods.py
@@ -1,9 +1,19 @@
+# encoding=utf-8
+
 class MyClass(object):
     def __init__(self):
         pass
- 
+
     def printMsg(self,msg):
         import os
         os.system('mkdir /tmp/lxp.log')
         print "hello "+msg
 
+    def writetbLog(self,title,quanter,integer,decimal,sale_count,img,code):
+        title,quanter,integer,decimal,sale_count,img,code = title,str(quanter),str(integer),str(decimal),str(sale_count),img,code
+        file = r'/tmp/tb.log'
+        with open(file, 'w+') as f:
+                f.write('"%s","%s","%s","%s","%s","%s","%s"\n'%(title,quanter,integer,decimal,sale_count,img,code))
+        #import os
+        #os.system('mkdir /tmp/lxp.log')
+        #print "hello "+msg
