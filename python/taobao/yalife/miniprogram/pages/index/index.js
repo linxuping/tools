ba1fc56e32ad0dc76c2d022778e06f1469b7125d
linxuping@126.com <linxuping@126.com>
2018-10-18 17:01:52 +0800

    update

--- python/taobao/yalife/miniprogram/pages/index/index.js
+++ python/taobao/yalife/miniprogram/pages/index/index.js
@@ -10,6 +10,20 @@ function sort(arr){
   return arr.sort(function () { return 0.5 - Math.random() });
 }
 
+function yesterday(num, str) {
+  var today = new Date();
+  var nowTime = today.getTime();
+  var ms = 24 * 3600 * 1000 * num;
+  today.setTime(parseInt(nowTime + ms));
+  var oYear = today.getFullYear();
+  var oMoth = (today.getMonth() + 1).toString();
+  if (oMoth.length <= 1) oMoth = '0' + oMoth;
+  var oDay = today.getDate().toString();
+  if (oDay.length <= 1) oDay = '0' + oDay;
+  //return oYear + str + oMoth + str + oDay;
+  return oMoth + "月" + oDay + "日";
+}
+
 function formatDate(time) {
   var date = new Date(time);
 
