e4ae97e5c0cfd1ad7f7104adfafa96c3ed85c56d
linxuping@126.com <linxuping@126.com>
2018-10-22 14:23:52 +0800

    python singleton

---
+++ python/test_singleton.py
@@ -0,0 +1,36 @@
+#encoding: utf-8
+#doc http://python.jobbole.com/87294/
+class Normal():
+	m = 2
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+	print "Singleton __call__"
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+    def __new__(cls, *args, **kwargs):
+	print "Singleton __new__"
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__new__(cls, *args, **kwargs)
+        return cls._instances[cls]
+ 
+
+# Python2
+class MyClass(object):
+    __metaclass__ = Singleton
+ 
+# Python3
+# class MyClass(metaclass=Singleton):
+#    pass
+
+def test_equal(c1, c2):
+	print id(c1), id(c2), c1==c2, c1 is c2 
+'''
+__new__出实例obj，然后多次__call__均返回obj
+'''
+test_equal(MyClass(),MyClass())
+test_equal(Normal(),Normal())
+print MyClass().__class__
