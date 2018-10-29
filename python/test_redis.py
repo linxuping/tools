d328a27bd2aac8a95fbbc4e0f7c089ab38e8a371
linxuping <linxuping@126.com>
2018-10-29 14:16:50 +0800

    Update test_redis.py

--- python/test_redis.py
+++ python/test_redis.py
@@ -1,4 +1,54 @@
+#encoding:utf-8
+import os
 import redis
 r = redis.Redis(host='127.0.0.1', port=6379, db=1)
-for i in r.hscan_iter(match="*circle*"):
-    print i
+#for i in r.hscan_iter(match="*circle*"):
+#    print i
+
+from redis import WatchError
+from concurrent.futures import ProcessPoolExecutor,ThreadPoolExecutor
+
+# 减库存函数, 循环直到减库存完成
+# 库存充足, 减库存成功, 返回True
+# 库存不足, 减库存失败, 返回False
+def decr_stock():
+
+    # python中redis事务是通过pipeline的封装实现的
+    with r.pipeline() as pipe:
+        while True:
+            try:
+                # watch库存键, multi后如果该key被其他客户端改变, 事务操作会抛出WatchError异常
+                pipe.watch('stock:count')
+                count = int(pipe.get('stock:count'))
+                if count > 0:  # 有库存
+                    # 事务开始
+                    pipe.multi()
+                    pipe.decr('stock:count')
+                    # 把命令推送过去
+                    # execute返回命令执行结果列表, 这里只有一个decr返回当前值
+                    print os.getpid(),pipe.execute()[0]
+                    return True
+                else:
+                    return False
+            except WatchError, ex:
+                # 打印WatchError异常, 观察被watch锁住的情况
+                print 'Error: ',ex
+                pipe.unwatch()
+
+
+def worker():
+    while True:
+        # 没有库存就退出
+        if not decr_stock():
+            break
+
+
+# 实验开始
+# 设置库存为100
+r.set("stock:count", 10)
+
+# 多进程模拟多个客户端提交
+#with ThreadPoolExecutor(max_workers=2) as pool:
+with ProcessPoolExecutor(max_workers=2) as pool:
+    for _ in range(10):
+        pool.submit(worker)
