11e940651981747b84bd5f8bf673f6675a1708e6
linxuping <linxuping@126.com>
2018-11-25 20:52:41 +0800

    decorator

--- test_decorator.py
+++ test_decorator.py
@@ -1,3 +1,4 @@
+#encoding=utf-8
 '''
  simple framework.
 '''
@@ -23,8 +24,27 @@ def my_processor(arg, args, kwargs):
 	print "args: ",args
 	print "kwargs: ",kwargs
 
-
 #do
 my_processor(100, "args_1", "args_2", key1=11, key2=22)
 #while.
+
+'''带参
+'''
+def wrapper0(dbtype):
+	def wrapped1(func):
+		def wrapped2(*args):
+			func(dbtype, *args)
+		return wrapped2
+	return wrapped1
+
+@wrapper0("main")
+def my_processor2(dbtype,x,y,z,u,v,w=None):
+	print dbtype, x,y,z,u,v
+
+my_processor2(1,2,3,'a','b')
+
+
+
+
+
 
