d72e7b7b629923bd08c376558bdb7d6f6ec7de93
linxuping <linxuping@126.com>
2019-05-25 09:54:41 +0800

    tree depth

---
+++ test_tree.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int m;
+    Node *left;
+    Node *right;
+};
+
+struct Tree {
+    Node *root;
+};
+
+void visit_tree(Node *node) {
+    if (node == NULL) {
+        printf("visit tree fin.\n");
+        return;
+    }
+    printf("%d\n",node->m);
+    if (node->left) {
+        visit_tree(node->left);
+    }
+    else if (node->right) {
+        visit_tree(node->right);
+    }
+}
+
+int get_tree_len(Node *node, int depth) {
+    if (node == NULL) {
+        printf("visit tree fin.\n");
+        return 0;
+    }
+    printf("%d\n",node->m);
+    int dep = 0;
+    if (node->left) {
+        dep = get_tree_len(node->left, depth+1);
+    }
+    else if (node->right) {
+        dep = get_tree_len(node->right, depth+1);
+    }
+    return dep>depth? dep:depth;
+}
+
+int get_tree_len2(Node *node) {
+    if (node == NULL) {
+        printf("visit tree fin.\n");
+        return 0;
+    }
+    printf("%d\n",node->m);
+    int depl = get_tree_len2(node->left);
+    int depr = get_tree_len2(node->right);
+    return max(depl,depr)+1;
+}
+
+int main(){
+    printf("123\n"); 
+    Node *root = new Node();
+    root->m = 0;
+    Node *n1 = new Node();
+    n1->m = 1;
+    Node *n2 = new Node();
+    n2->m = 2;
+    Node *n3 = new Node();
+    n3->m = 3;
+    Node *n4 = new Node();
+    n4->m = 40;
+    Node *n5 = new Node();
+    n5->m = 5;
+
+    root->left = n1;
+    root->right = n2;
+
+    n1->right = n3;
+
+    n3->left = n4;
+
+    n4->left = n5;
+
+    visit_tree(root);
+    int len = get_tree_len(root, 0);
+    printf("depth: %d\n", len);
+    int len2 = get_tree_len2(root);
+    printf("depth 2: %d\n", len2);
+}
